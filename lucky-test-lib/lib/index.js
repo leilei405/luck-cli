977b471f30a2e3443161afb2f4b5188c8cca7aa4
leilei405 <1601178425@qq.com>
2024-12-04 22:44:35 +0800

    feat: ✨ 在test中添加发布的远程lib包

--- lucky-test-lib/lib/index.js
+++ lucky-test-lib/lib/index.js
@@ -2,4 +2,7 @@ module.exports = {
   sum(a, b) {
     return a + b;
   },
+  mul() {
+    return 100;
+  },
 };
--- lucky-test/bin/index.js
+++ lucky-test/bin/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const lib = require("lucky-test-lib");
+
 console.log(lib.sum(1, 5), "==lib==");
 
 console.log("Hello World! Test");
