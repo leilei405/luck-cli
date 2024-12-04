e2580062b58cfa18c4d1d601685efdaf6a9b75af
leilei405 <1601178425@qq.com>
2024-12-04 23:50:04 +0800

    feat: ✨ 调试

--- lucky-test-lib/lib/index.js
+++ lucky-test-lib/lib/index.js
@@ -5,4 +5,16 @@ module.exports = {
   mul() {
     return 100;
   },
+  init({ option, param }) {
+    console.log("执行init初始化流程", option, param);
+  },
+  publish() {
+    console.log("执行publish 发布流程");
+  },
+  version() {
+    console.log("执行version版本流程");
+  },
+  build() {
+    console.log("执行build构建流程");
+  },
 };
--- lucky-test/bin/index.js
+++ lucky-test/bin/index.js
@@ -1,6 +1,31 @@
 #!/usr/bin/env node
 const lib = require("lucky-test-lib");
+const params = require("process").argv;
+console.log(params, "全部参数");
 
-console.log(lib.sum(1, 5), "==lib==");
+// 1. 注册一个命令 lucky-test init
+const command = params[2];
 
-console.log("Hello World! Test");
+const options = params.slice(3);
+if (options.length > 1) {
+  let [option, param] = options;
+  option = option.replace(/-/g, "");
+
+  if (lib[command]) {
+    lib[command]({ option, param });
+  } else {
+    console.log(`${command} 指令不存在`);
+  }
+}
+
+console.log(command, "command");
+
+// 2. 实现参数解析 --version 和 init --name
+if (command.startsWith("--") || command.startsWith("-")) {
+  let globalOption = command.replace(/--|-/g, "");
+  if (globalOption === "version" || globalOption === "V") {
+    console.log("版本: 1.0.0");
+  } else {
+    console.log(`${globalOption} 指令不存在`);
+  }
+}
