2434d6a1675584e689f59e8b20ef1d49b4df6622
leilei405 <1601178425@qq.com>
2024-12-06 22:10:08 +0800

    feat: ✨commander 快速入门 & 简单使用

--- lucky-test/bin/index.js
+++ lucky-test/bin/index.js
@@ -1,121 +1,38 @@
 #!/usr/bin/env node
-// const lib = require("lucky-test-lib");
-// const params = require("process").argv;
-// console.log(params, "全部参数");
 
-// // 1. 注册一个命令 lucky-test init
-// const command = params[2];
+const commander = require("commander");
+const pkg = require("../package.json");
 
-// const options = params.slice(3);
-// if (options.length > 1) {
-//   let [option, param] = options;
-//   option = option.replace(/-/g, "");
+// 获取commander的单例
+// const { program } = commander;
 
-//   if (lib[command]) {
-//     lib[command]({ option, param });
-//   } else {
-//     console.log(`${command} 指令不存在`);
-//   }
-// }
+// 实例化commander
+const program = new commander.Command();
 
-// console.log(command, "command");
+program
+  // 命令名称 会自动获取的 会自动获取package.json中的bin字段的key值
+  .name(Object.keys(pkg.bin)[0])
 
-// // 2. 实现参数解析 --version 和 init --name
-// if (command?.startsWith("--") || command?.startsWith("-")) {
-//   let globalOption = command.replace(/--|-/g, "");
-//   if (globalOption === "version" || globalOption === "V") {
-//     console.log("版本: 1.0.0");
-//   } else {
-//     console.log(`${globalOption} 指令不存在`);
-//   }
-// }
-const yargs = require("yargs/yargs");
-const dedent = require("dedent");
-const { hideBin } = require("yargs/helpers");
+  // 编写帮助信息
+  .usage("<command> [options]")
 
-const argv = hideBin(process.argv);
+  // option 选项配置 简写 -d 或者 --debug
+  .option("-d, --debug", "查看一些信息日志, 是否启动调试模式, 默认为false")
+  .option("-e, --env <env>", "获取查看当前的环境变量")
 
-const cli = yargs(argv);
+  // 获取当前的版本号
+  .version(pkg.version)
 
-cli
-  // 使用说明
-  .usage("请使用: $0 <命令>/<command> [参数]/[options]")
-  .demandCommand(1, "请最少输入一个参数来执行此命令")
+  // 解析参数
+  .parse(process.argv);
 
-  // 如果我们输入的命令不存在，会给我们推荐一些命令，从指令列表中去匹配比较相近的命令 提示出来
-  .recommendCommands()
+// 执行 lucky-test 时候直接调出帮助信息
+program.outputHelp();
 
-  // 监听错误
-  .fail((msg, err, yargs) => {
-    console.log(msg, "msg");
-    console.log(err, "err");
-  })
+// 打印出出Options
+console.log(program.opts(), "program.opts()");
 
-  // 注册命令
-  .command(
-    "init [name]",
-    "初始化一个项目",
-    (yargs) => {
-      yargs.option("name", {
-        type: "string",
-        describe: "项目名称",
-        alias: "n",
-      });
-    },
-    (argReset) => {
-      console.log(argReset, "argReset1");
-    }
-  )
-  .command({
-    command: "list",
-    aliases: ["ls", "la", "ll"],
-    describe: "查看项目列表",
-    builder: (yargs) => {
-      yargs.option("name", {
-        type: "string",
-        describe: "项目名称",
-        alias: "n",
-      });
-    },
-    handler: (argReset) => {
-      console.log(argReset, "argReset2");
-    },
-  })
-
-  // 给命令注册起一个别名
-  .alias("v", "version")
-  .alias("h", "help")
-
-  // 设定控制台信息展示的一个宽度 terminalWidth表示铺满
-  .wrap(cli.terminalWidth())
-
-  // 注册命令的描述信息 注册多个
-  .options({
-    debug: {
-      type: "boolean",
-      describe: "查看一些调试信息(log)日志",
-      alias: "d",
-    },
-  })
-
-  // 命令一个一个注册
-  .option("registry", {
-    type: "string",
-    describe: "请输入你的名字",
-    alias: "r",
-  })
-
-  // 给我们的命令进行分组
-  .group(["debug"], "Debug Options")
-  .group(["registry"], "Register Options")
-  .group(["help"], "Help Options")
-  .group(["version"], "Version Options")
-
-  // 在底部加一段说明
-  .epilogue(
-    dedent`用起来非常香的一个库、欢迎使用（食用）欢迎大家使用！！！
-    Lucky出版必属精品、欢迎大家！！！`
-  )
-
-  // 严格模式，不允许输入不存在的命令
-  .strict().argv;
+// 执行 lucky-test --version
+// 执行 lucky-test -V
+// 执行 lucky-test --help
+// 执行 lucky-test -h
