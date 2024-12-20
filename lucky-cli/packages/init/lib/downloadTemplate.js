c70485f7677986e5747c2bbc06270675caf39927
leilei405 <1601178425@qq.com>
2024-12-20 22:22:07 +0800

    feat: ✨ 下载项目模逻辑开发

--- lucky-cli/packages/init/lib/downloadTemplate.js
+++ lucky-cli/packages/init/lib/downloadTemplate.js
@@ -2,14 +2,15 @@ import path from "node:path";
 import { pathExistsSync } from "path-exists";
 import fse from "fs-extra";
 import ora from "ora";
-import { printErrorLog } from "@lucky.com/utils";
+import { execa } from "execa";
+import { log, printErrorLog } from "@lucky.com/utils";
 
 // 1. 先获取缓存目录
 const getCacheDir = (targetPath) => {
   return path.resolve(targetPath, "node_modules");
 };
 
-// 2.
+// 2. 下载模版
 const makeCacheDir = (targetPath) => {
   const cacheDir = getCacheDir(targetPath);
   if (!pathExistsSync(cacheDir)) {
@@ -17,14 +18,27 @@ const makeCacheDir = (targetPath) => {
   }
 };
 
-export default function downloadTemplate(selectedTemplate) {
+// 3. 下载模版 先拿到path 再下载
+const downloadAddTemplate = async (targetPath, selectedTemplate) => {
+  const { npmName, version } = selectedTemplate;
+  const installCommand = "npm";
+  const installArgs = ["install", `${npmName}@${version}`];
+  const cwd = targetPath;
+  log.verbose("installCommand", installArgs);
+  log.verbose("cwd", cwd);
+  await execa(installCommand, installArgs, {
+    cwd,
+    stdio: "inherit",
+  });
+};
+
+export default async function downloadTemplate(selectedTemplate) {
   // 1. 下载模版 先拿到path 再下载
   const { targetPath, template } = selectedTemplate;
   const spinner = ora("正在下载模版...").start();
   try {
-    setTimeout(() => {
-      spinner.stop();
-    }, 2000);
+    await downloadAddTemplate(targetPath, template);
+    spinner.stop();
     spinner.succeed("模版下载成功");
   } catch (error) {
     spinner.fail("模版下载失败");
