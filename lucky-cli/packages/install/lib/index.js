437565b26441f1228f9ada964d7c348b25017e53
leilei405 <1601178425@qq.com>
2024-12-25 23:53:57 +0800

    feat: ✨ 实现搜索列表和翻页功能

--- lucky-cli/packages/install/lib/index.js
+++ lucky-cli/packages/install/lib/index.js
@@ -7,6 +7,10 @@ import {
   Gitee,
   makeInput,
 } from "@lucky.com/utils";
+
+const PREV_PAGE = "${prev_page}"; // 上一页
+const NEXT_PAGE = "${next_page}"; // 下一页
+
 class InstallCommand extends Command {
   get command() {
     return "install";
@@ -55,7 +59,7 @@ class InstallCommand extends Command {
 
   async searchGitApi() {
     // 关键字
-    const keyword = await makeInput({
+    this.searchKeyword = await makeInput({
       message: "请输入搜索关键字",
       validate: (val) => {
         if (!val) {
@@ -64,45 +68,114 @@ class InstallCommand extends Command {
         return true;
       },
     });
-
     // 编程语言
-    const language = await makeInput({
+    this.language = await makeInput({
       message: "请输入搜索语言",
     });
+    this.page = 1; // 当前页码
+    this.perPage = 2; // 每页条数
 
-    log.verbose(language, "language", keyword, "keyword");
-    log.verbose(this.gitAPI.getPlatform(), "getPlatform");
+    // log日志
+    log.verbose(
+      "language",
+      this.language,
+      "searchKeyword",
+      this.searchKeyword,
+      "getPlatform",
+      this.gitAPI.getPlatform()
+    );
 
+    await this.doSearch();
+  }
+
+  async doSearch() {
     const platform = this.gitAPI.getPlatform();
     let searchResult;
-    this.page = 1;
+    let totalCount = 0;
+    let projectList = [];
 
     // 判断走 GITEE 还是 GITHUB
     if (platform === "github") {
       const params = {
-        q: keyword + (language ? `+language:${language}` : ""),
+        q:
+          this.searchKeyword +
+          (this.language ? `+language:${this.language}` : ""),
         order: "desc",
-        per_page: 2,
-        page: 1,
-        sort: "count",
+        per_page: this.perPage,
+        page: this.page,
+        sort: "stars",
       };
-      console.log(params, "githubParams");
+      log.verbose("githubParams", params);
       searchResult = await this.gitAPI.searchRepositories(params);
+      totalCount = searchResult.total_count; // 总条数
+      projectList = searchResult.items.map((item) => {
+        return {
+          name: `项目名称：${item.name} ＋ 描述：${item.description}`,
+          value: item.full_name,
+        };
+      });
+    }
+
+    // 判断当前页面是否为最大页数
+    if (this.page * this.perPage < totalCount) {
+      projectList.push({
+        name: "下一页",
+        value: NEXT_PAGE,
+      });
+    }
+    if (this.page > 1) {
+      projectList.unshift({
+        name: "上一页",
+        value: PREV_PAGE,
+      });
+    }
+
+    const keyword = await makeList({
+      message: `请选择要下载的项目，(共 ${totalCount} 条数据)`,
+      choices: projectList,
+    });
+
+    if (keyword === NEXT_PAGE) {
+      await this.nextPage();
+    } else if (keyword === PREV_PAGE) {
+      await this.prevPage();
+    } else {
+      // 下载项目
     }
 
     if (platform === "gitee") {
       const params = {
         q: keyword + (language ? `+language:${language}` : ""),
         order: "desc",
-        per_page: 5,
-        page: 1,
+        per_page: this.perPage,
+        page: this.page,
         sort: "stars_count",
       };
-      console.log(params, "giteeParams");
+      log.verbose("giteeParams", params);
       searchResult = await this.gitAPI.searchRepositories(params);
+      projectList = searchResult.items.map((item) => {
+        return {
+          name: `项目名称：${item.name} ＋ 描述 ：${item.description}`,
+          value: item.full_name,
+        };
+      });
+      totalCount = searchResult.total_count; // 总条数
     }
-
+    log.verbose("totalCount", totalCount);
     console.log(searchResult);
+    console.log(projectList, "===projectList==");
+  }
+
+  // 下一页
+  async nextPage() {
+    this.page++;
+    await this.doSearch();
+  }
+
+  // 上一页
+  async prevPage() {
+    this.page--;
+    await this.doSearch();
   }
 }
 
